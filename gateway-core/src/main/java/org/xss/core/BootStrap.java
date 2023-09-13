180a804172d2b8f773a79f7c750c1c4e3609b312
XSS <2364704998@qq.com>
2023-09-13 10:23:59 +0800

    init the gateway-api project

---
+++ gateway-core/src/main/java/org/xss/core/BootStrap.java
@@ -0,0 +1,19 @@
+package org.xss.core;
+
+/**
+ * 核心配置启动类
+ */
+public class BootStrap {
+    public static void main(String[] args) {
+        /**
+         *加载网关静态配置
+         * 插件初始化
+         * 配置中心管理器初始化，连接配置中心，监听配置新增、修改、删除
+         * 启动容器
+         * 连接注册中心、将注册中心实例加载到本地
+         * 服务优雅关机
+         */
+
+
+    }
+}
